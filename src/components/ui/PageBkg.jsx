fda21dbfa26d871fb56b5653c11ac36f4435db2d
magb7 <margaux.bedu@gmail.com>
2020-07-03 18:11:02 +0200

    remove useless styled component

--- src/components/ui/PageBkg.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  background: linear-gradient(${(props) => props.theme.colors.gradient});
-  width: 100vw;
-  height: 100vh;
-  color: ${(props) => props.theme.colors.primary};
-`;
