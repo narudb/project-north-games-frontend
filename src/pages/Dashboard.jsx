c8e61bf8b588cb39990d234e97dff9eff67bfbec
ValentinV3 <valentin.vantroys@gmail.com>
2020-07-06 12:18:19 +0200

    Create the News
    Card component and add gradiant color in GlobalTheme

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TitleStyle from '../components/ui/Title';
+import NewsCard from '../components/NewsCard';
 
 const DashboardWrapper = styled.div`
   width: 100%;
@@ -12,6 +13,7 @@ const Dashboard = () => {
   return (
     <DashboardWrapper>
       <TitleStyle>Actu à la une</TitleStyle>
+      <NewsCard />
       <p>Dashboard Page</p>
     </DashboardWrapper>
   );
--- src/theme/globalTheme.jsx
+++ src/theme/globalTheme.jsx
@@ -2,7 +2,8 @@ export default {
   colors: {
     primary: '#fff',
     secondary: '#3191cf',
-    gradient: '180deg, #146CBD 0%, #146CBD 0.01%, #0B2687 100%',
+    bckgGradient: '180deg, #146CBD 0%, #146CBD 0.01%, #0B2687 100%',
+    newsCardGradient: '35.75deg, #FCA044 1.97%, #FE757B 96.85%',
     black: '#000',
     mediumGray: '#606b72',
   },
