b928dd59b30bf5e67b3cc436e69fb5caba31278d
magb7 <margaux.bedu@gmail.com>
2020-07-08 16:55:27 +0200

    add grid style to structured the page and import form components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import TitleStyle from '../components/ui/Title';
-import NewsCard from '../components/NewsCard';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
+import ButtonStyle from '../components/ui/Button';
 
 const DashboardWrapper = styled.div`
   width: 100%;
   height: 100%;
-  padding: 15px;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'news form'
+    'news form'
+    '. .'
+    'events aside'
+    'events aside';
+`;
+
+const FormWrapper = styled.div`
+  grid-area: form;
+  border: 1px solid purple;
+  display: flex;
+  flex-flow: column wrap;
+  padding: 10px;
 `;
 
 const Dashboard = () => {
+  const [showSignup, toggleForm] = useState(true);
+
   return (
     <DashboardWrapper>
-      <TitleStyle>Actu à la une</TitleStyle>
-      <NewsCard />
       <p>Dashboard Page</p>
+      <FormWrapper>
+        {showSignup ? (
+          <>
+            <SignUp />
+            <ButtonStyle
+              type="button"
+              onClick={() => {
+                toggleForm(false);
+              }}
+            >
+              J&apos;ai déjà un compte !
+            </ButtonStyle>
+          </>
+        ) : (
+          <>
+            <SignIn />
+            <ButtonStyle
+              type="button"
+              onClick={() => {
+                toggleForm(true);
+              }}
+            >
+              Je peux m&apos;inscrire ?
+            </ButtonStyle>
+          </>
+        )}
+      </FormWrapper>
     </DashboardWrapper>
   );
 };
