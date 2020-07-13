73414f1996be5c5abff8a4003c3c731f102e2334
magb7 <margaux.bedu@gmail.com>
2020-07-13 17:09:37 +0200

    implement basic form validation and axios

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import Title from './ui/Title';
 import Form from './ui/Form';
 import Input from './ui/Input';
@@ -9,16 +10,61 @@ const SignUpWrapper = styled.div`
   height: 90%;
 `;
 
+const StyledSpan = styled.span`
+  color: red;
+`;
+
 const SignUp = () => {
   const [newUser, setNewUser] = useState({});
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    mail: '',
+    password: '',
+  });
+  const [valide, setValide] = useState(true);
+
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+
+  const formValid = (form) => {
+    Object.values(form).forEach((val) => {
+      if (val.length < 0) {
+        setValide(false);
+      }
+    });
+    return valide;
+  };
 
   const handleChange = (e) => {
-    const tmp = { ...newUser, [e.target.name]: e.target.value };
-    setNewUser(tmp);
+    const { name, value } = e.target;
+    const tmpErrors = { ...formErrors };
+
+    switch (name) {
+      case 'name':
+        tmpErrors.name = value.length < 3 ? 'minimum 3 caractères' : '';
+        break;
+      case 'mail':
+        tmpErrors.mail = emailRegex.test(value)
+          ? ''
+          : 'addresse mail non valide';
+        break;
+      case 'password':
+        tmpErrors.password = value.length < 8 ? 'minimum 8 caractères' : '';
+        break;
+      default:
+        break;
+    }
+
+    setFormErrors(tmpErrors);
+    setNewUser({ ...newUser, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formValid(formErrors)) {
+      axios.post(`http://localhost:5000/auth/signup`, newUser);
+    }
   };
 
   return (
@@ -29,47 +75,48 @@ const SignUp = () => {
           handleSubmit(e);
         }}
       >
-        <Input
-          type="text"
-          name="name"
-          id="name"
-          required
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          placeholder="Ton Nom"
-        />
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          required
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          placeholder="Ton Email"
-        />
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          required
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          placeholder="Ton mot de passe"
-        />
-        <Input
-          type="password"
-          name="passwordbis"
-          id="passwordbis"
-          required
-          // Maybe not usefull ask Loïc?
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          placeholder="Confirme ton mot de passe"
-        />
+        <>
+          <Input
+            type="text"
+            name="name"
+            required
+            onBlur={(e) => {
+              handleChange(e);
+            }}
+            placeholder="Ton Nom"
+          />
+          {formErrors.name.length > 0 && (
+            <StyledSpan>{formErrors.name}</StyledSpan>
+          )}
+        </>
+        <>
+          <Input
+            type="email"
+            name="mail"
+            required
+            onBlur={(e) => {
+              handleChange(e);
+            }}
+            placeholder="Ton Email"
+          />
+          {formErrors.mail.length > 0 && (
+            <StyledSpan>{formErrors.mail}</StyledSpan>
+          )}
+        </>
+        <>
+          <Input
+            type="password"
+            name="password"
+            required
+            onBlur={(e) => {
+              handleChange(e);
+            }}
+            placeholder="Ton mot de passe"
+          />
+          {formErrors.password.length > 0 && (
+            <StyledSpan>{formErrors.password}</StyledSpan>
+          )}
+        </>
         <InputBtn type="submit" value="Je m'inscris" />
       </Form>
     </SignUpWrapper>
