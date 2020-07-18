bbbc1748647aee991f332f8b0e212e84f3c49297
magb7 <margaux.bedu@gmail.com>
2020-07-18 12:40:19 +0200

    create a new component to display user name and avatar while authenticate

---
+++ src/components/UserIdentity.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import axios from 'axios';
+import { backend } from '../conf';
+import globalTheme from '../theme/globalTheme';
+
+const Avatar = styled.img`
+  height: 8vh;
+  width: 4vw;
+  border-radius: 50%;
+`;
+const Username = styled.p`
+  font-family: ${(props) => props.theme.fonts.primary};
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  text-transform: uppercase;
+  margin-top: 15px;
+`;
+const UserWrapper = styled.div`
+  width: 15vw;
+  display: flex;
+  justify-content: space-evenly;
+  margin-right: 20px;
+`;
+
+const UserIdentity = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userReducer.userData);
+  const authData = useSelector((state) => state.userReducer.authData);
+
+  useEffect(() => {
+    axios
+      .get(`${backend}/users/${authData.id}`, {
+        headers: {
+          Authorization: `Bearer ${authData.token || null}`,
+        },
+      })
+      .then(({ data }) => {
+        dispatch({
+          type: 'GET_USER',
+          payload: data,
+        });
+      });
+  });
+
+  return (
+    <UserWrapper>
+      <Avatar src={globalTheme.pictures.avatar} />
+      <Username>{userData.name}</Username>
+    </UserWrapper>
+  );
+};
+
+export default UserIdentity;
